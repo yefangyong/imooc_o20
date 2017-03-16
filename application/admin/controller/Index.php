315c327dced76429d25e820d185695030198d589
yefangyong <1281074511@qq.com>
2017-03-16 16:13:27 +0800

    前后台页面搭建

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,11 +1,16 @@
 <?php
-namespace app\index\controller;
+namespace app\admin\controller;
 use think\Controller;
-
 class Index extends Controller
 {
     public function index()
     {
         return $this->fetch();
     }
+
+    public function welcome() {
+        return '欢迎来到o2o商城平台!';
+    }
+
+
 }
